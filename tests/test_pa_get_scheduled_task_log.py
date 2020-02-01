6f48ae49045a32ec535952217c10515787d52f8b
Piotr Kaznowski <piotr.kaznowski@gmail.com>
2020-02-01 23:19:13 +0100

    fixes get scheduled task log

--- tests/test_pa_get_scheduled_task_log.py
+++ tests/test_pa_get_scheduled_task_log.py
@@ -7,13 +7,13 @@ from scripts.pa_get_scheduled_task_log import main
 
 @pytest.mark.tasks
 def test_calls_all_stuff_in_right_order(mocker):
-    mock_Task = mocker.patch("scripts.pa_get_scheduled_task_log.Task")
-    mock_Task.return_value.logfile = "/user/{username}/files/foo".format(
+    mock_task_from_id = mocker.patch("scripts.pa_get_scheduled_task_log.Task.from_id")
+    mock_task_from_id.return_value.logfile = "/user/{username}/files/foo".format(
         username=getpass.getuser()
     )
     mock_print = mocker.patch("builtins.print")
 
     main(42)
 
-    assert mock_Task.call_args == call(task_id=42)
+    assert mock_task_from_id.call_args == call(task_id=42)
     assert mock_print.call_args == call("/foo")
