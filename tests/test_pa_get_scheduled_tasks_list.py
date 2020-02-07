77fc359ae1b7f3c667e0d61a9bd0dc8f3914bd5e
Piotr Kaznowski <piotr.kaznowski@gmail.com>
2020-02-07 20:05:24 +0100

    updates tests for pa_get_scheduled_taskss_list

--- tests/test_pa_get_scheduled_tasks_list.py
+++ tests/test_pa_get_scheduled_tasks_list.py
@@ -3,55 +3,48 @@ from unittest.mock import call
 
 import pytest
 from scripts.pa_get_scheduled_tasks_list import main
+
 from pythonanywhere.task import Task
 
-username = getpass.getuser()
-example_specs = [
-    {
+
+@pytest.fixture
+def example_tasks_list():
+    username = getpass.getuser()
+    specs1 = {
         "can_enable": False,
         "command": "echo foo",
         "enabled": True,
         "expiry": None,
-        "extend_url": "/user/{username}/schedule/task/42/extend".format(username=username),
+        "extend_url": "/user/{}/schedule/task/42/extend".format(username),
         "hour": 16,
         "id": 42,
         "interval": "daily",
         "logfile": "/user/{username}/files/var/log/tasklog-126708-daily-at-1600-echo_foo.log",
         "minute": 0,
         "printable_time": "16:00",
-        "url": "/api/v0/user/{username}/schedule/42".format(username=username),
+        "url": "/api/v0/user/{}/schedule/42".format(username),
         "user": username,
-    },
-    {
-        "can_enable": False,
-        "command": "echo bar",
-        "enabled": False,
-        "expiry": None,
-        "extend_url": "/user/{username}/schedule/task/43/extend".format(username=username),
-        "hour": 17,
-        "id": 43,
-        "interval": "daily",
-        "logfile": "/user/{username}/files/var/log/tasklog-126709-daily-at-1710-echo_bar.log",
-        "minute": 10,
-        "printable_time": "17:10",
-        "url": "/api/v0/user/{username}/schedule/43".format(username=username),
-        "user": username,
-    },
-]
+    }
+    specs2 = {**specs1}
+    specs2.update({"command": "echo bar", "hour": 17, "minute": 43, "printable_time": "17:43"})
+    yield [specs1, specs2]
 
 
 @pytest.mark.tasks
-@pytest.mark.dev
-def test_calls_all_stuff_in_right_order(mocker):
+def test_logs_task_list_as_table(example_tasks_list, mocker):
+    tasks = [Task.from_specs(specs) for specs in example_tasks_list]
     mock_TaskList = mocker.patch("scripts.pa_get_scheduled_tasks_list.TaskList")
-    mock_TaskList.return_value.tasks = [Task.from_specs(specs) for specs in example_specs]
+    mock_TaskList.return_value.tasks = tasks
     mock_tabulate = mocker.patch("scripts.pa_get_scheduled_tasks_list.tabulate")
+    mock_logger = mocker.patch("scripts.pa_get_scheduled_tasks_list.get_logger")
 
     main(tablefmt="orgtbl")
 
     headers = "id", "interval", "at", "enabled", "command"
     attrs = "task_id", "interval", "printable_time", "enabled", "command"
-    tasks = [Task.from_specs(specs) for specs in example_specs]
     table = [[getattr(task, attr) for attr in attrs] for task in tasks]
+
     assert mock_TaskList.call_count == 1
     assert mock_tabulate.call_args == call(table, headers, tablefmt="orgtbl")
+    assert mock_logger.call_args == call(set_info=True)
+    assert mock_logger.return_value.info.call_count == 1
