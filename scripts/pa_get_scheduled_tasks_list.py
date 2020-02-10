47b247cebed36ec0b8c8486ee92601da2e319a4b
Piotr Kaznowski <piotr.kaznowski@gmail.com>
2020-02-10 21:36:34 +0000

    updates pa_get_list docstring

--- scripts/pa_get_scheduled_tasks_list.py
+++ scripts/pa_get_scheduled_tasks_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.5
 """Get list of user's scheduled tasks as a table with columns:
-id, interval, at (hour:minute/minute past), enabled, command.
+id, interval, at (hour:minute/minute past), status (enabled/disabled), command.
 
 Usage:
   pa_get_scheduled_tasks_list.py [--format TABLEFMT]
